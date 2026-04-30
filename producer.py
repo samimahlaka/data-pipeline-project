cb30cd6d06d3b81e77c90c1848798ffad86694ce
Mahlaka <samimahlaka@gmail.com>
2026-04-30 18:16:13 -0500

    add Redis stream integration to producer

--- producer.py
+++ producer.py
@@ -2,11 +2,19 @@ from faker import Faker
 import json
 import random
 import time
+import redis
 import random
 
 fake = Faker()
 event_types = ['click', 'view', 'purchase']
 
+redis_client = redis.Redis(
+    host = "localhost",
+    port = 6379,
+    decode_responses = True
+)
+
+STREAM_NAME = "user_events"
 def generate_events():
     event = {
         "event_type": random.choice(event_types),
@@ -16,10 +24,14 @@ def generate_events():
     }
     return event
     
-if __name__ == "main":
+if __name__ == "__main__":
     while True:
         event = generate_events()
-        json_event = json.dumps(event)
-        print(json_event)
+        
+        redis_client.xadd(
+            STREAM_NAME,
+            {"event" : json.dumps(event)}
+        )
+        print("Sent event:", event)
         time.sleep(1)
         
